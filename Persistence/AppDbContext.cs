a536e5aa031402f37de4f9da1466ca52d1cef0db
Dariel Martinez <clever.dmp@gmail.com>
2025-05-18 15:41:53 -0400

    First commit

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
